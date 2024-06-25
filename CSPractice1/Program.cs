4bde7450fd35036c012f5a1b36f3ee1aa69ed8b2
thdud__ <sylim1114y@gmail.com>
2024-06-25 10:21:00 +0900

    #1 12-3. 기본 자료형

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -21,6 +21,44 @@ namespace CSPractice1
             //출력
             Console.WriteLine("Hello World");
             Console.Write("뉴라인X");
+
+            //#1 12-3. 기본자료형
+            //정수
+            Console.WriteLine(52 + 4 - 1);
+            //사칙연산자 = 산술연산자 = Arithmetic Operator
+            //+ - * / %
+
+            //실수(real number)
+            Console.WriteLine(52.0);
+
+            //문자(Character)
+            Console.WriteLine('A');
+            Console.WriteLine('가');
+
+            //문자열(String)
+            Console.WriteLine("Hello World");
+            //이스케이프 문자
+            Console.WriteLine("미\n\\림\t마\"고");
+
+            //문자열 연결 연산자(Concatenations Operator)
+            Console.WriteLine("Hello" + " " + "World");
+            //문자열 인덱스 접근 []
+            Console.WriteLine("Hello World"[4]);    //'o'
+            Console.WriteLine("Hello World"[100]);  //예외발생
+
+            //주의
+            Console.WriteLine('가' + '힣');   //문자열 연결 연산자xx -> 덧셈연산자로 인식
+
+            //논리(boolean)
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            //비교연산자(Comparison Operator)
+            //== !- > < >= <=
+            //논리연산자(logical operator)
+            //&&||!
+            Console.WriteLine(!true);
+            Console.WriteLine(true && false);
+            Console.WriteLine(true || false);
         }
     }
 }
