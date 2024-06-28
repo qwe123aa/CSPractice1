4126e09284f3e39d852db224bb89a932883f3712
thdud__ <sylim1114y@gmail.com>
2024-06-28 10:00:55 +0900

    #3 14-1. 반복문과 배열

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -115,6 +115,9 @@ namespace CSPractice1
             // #2 13-6. 삼항연산자
             Console.WriteLine(number % 2 = 0 ? "짝수" : "홀수");
 
+            // #3 14-1. 반복문과 배열
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray[99]);
 
 
 
