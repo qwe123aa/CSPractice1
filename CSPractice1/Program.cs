737e24fda2a902e7d863e38c90848edcdee3cb1e
thdud__ <sylim1114y@gmail.com>
2024-07-01 13:46:24 +0900

    #5 22-3. 클래스 매서드

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class MyMath
+    {
+        public static int classVar = 1;
+        public int instanceVar = 2;
+        public static int Abs(int input)
+        {
+            Console.WriteLine(classVar);
+            //클래스 매서드에서 인스턴스 변수 접근 불가
+            //Console.WriteLine(instanceVar);
+            return (input >= 0) ? input : -input;
+        }
+    }
     class FirstClass
     {
 
@@ -178,6 +190,8 @@ namespace CSPractice1
             //#4  21-5. 클래스 변수
             Console.WriteLine(Product.TAX_RATIO);
             //Console.WriteLine(p1.TAX_RATIO);
+
+            // #5 22-3. 클래스 매서드
         }
     }
 }
