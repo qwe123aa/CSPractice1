2c788d723b76be0bfe716b4ee4e133e2682d474c
thdud__ <sylim1114y@gmail.com>
2024-07-01 13:40:42 +0900

    //#4  21-5. 클래스 변수

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -175,6 +175,9 @@ namespace CSPractice1
             Product p2 = new Product() { price = 1000000, name = "아이폰" };
             Product p3 = new Product() { price = 1000000 };
 
+            //#4  21-5. 클래스 변수
+            Console.WriteLine(Product.TAX_RATIO);
+            //Console.WriteLine(p1.TAX_RATIO);
         }
     }
 }
